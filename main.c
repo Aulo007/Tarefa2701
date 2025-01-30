d31b80ddcb2d21b7a24c8b1899dc736dc8dd655a
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-30 17:21:17 -0300

    Implementa debounce para evitar ruídos na leitura do botão

--- main.c
+++ main.c
@@ -10,7 +10,12 @@ const uint LED_RED_PIN = 13; // Green = 11, Blue = 12 and Red = 13
 const uint BUTTON1_PIN = 5; // Define o botão 1 como pino 5
 const uint BUTTON2_PIN = 6; // Define o botão 2 como pino 6
 
-volatile int num; // Variável global para definir qual número selecionar na matriz
+// Definição de variáveis globais
+
+static volatile int num;           // Variável global para definir qual número selecionar na matriz
+static volatile int num1 = 1;      // Variável global para testar doubounce
+static volatile int num2 = 1;      // Variável global para testar doubounce
+static volatile int last_time = 0; // Variável global para amarzenar o tempo do último evento
 
 static void gpio_irq_handle(uint gpio, uint32_t events);
 
@@ -59,29 +64,44 @@ int main()
         sleep_ms(100);
         gpio_put(LED_RED_PIN, false);
         sleep_ms(100);
-
-        printf("Botão 1: %d \n", gpio_get(LED_RED_PIN));
     }
 }
 
 static void gpio_irq_handle(uint gpio, uint32_t events)
 {
-    if (num >= 0 && num <= 9)
+    // Cria uma varíavel que pega o tempo atual do sistema em microsegundos.
+    uint32_t current_time_us = to_us_since_boot(get_absolute_time());
+
+    // Print para debbugar o efeito do doubounce
+
+    printf("Sinais de transição lidos pelo sistema: %d \n", num1);
+    num1++;
+
+    if (current_time_us - last_time > 200000) // 200 ms de doboucing
     {
-        if (gpio == BUTTON1_PIN)
+        last_time = current_time_us; // atualiza o tempo do último evento.
+        if (num >= 0 && num <= 9)
         {
-            /* Ternário (Condição ? valor se verdadeiro: valor se falso), atribui valor a variável volátil num
-               Se o número for menor que 9 ele incrementa o num em 1, se não, faz o valor permanecer em 9 */
+            if (gpio == BUTTON1_PIN)
+            {
+                /* Ternário (Condição ? valor se verdadeiro: valor se falso), atribui valor a variável volátil num
+                   Se o número for menor que 9 ele incrementa o num em 1, se não, faz o valor permanecer em 9 */
 
-            num = (num < 9) ? num + 1 : 9;
-        }
-        else if (gpio == BUTTON2_PIN)
-        {
-            /* Ternário (Condição ? valor se verdadeiro: valor se falso), atribui valor a variável volátil num
-              Se o número for maior que 0 ele decrementa o num em 1, se não, faz o valor permanecer em 0 */
-            num = (num > 0) ? num - 1 : 0;
-        }
+                num = (num < 9) ? num + 1 : 9;
+            }
+            else if (gpio == BUTTON2_PIN)
+            {
+                /* Ternário (Condição ? valor se verdadeiro: valor se falso), atribui valor a variável volátil num
+                  Se o número for maior que 0 ele decrementa o num em 1, se não, faz o valor permanecer em 0 */
+                num = (num > 0) ? num - 1 : 0;
+            }
 
-        setMatrizDeLEDSComIntensidade(caixa_de_desenhos[num], 0.1, 0.1, 0.1);
+            // Print para mostrar quantas vezes eu apertei o botão na plaquinha e realmente rodou o código
+            printf("O botão foi realmente apertado: %d \n", num2);
+            num2++;
+
+            // Escreve desenho atual na plaquinha, os três parâmetro são sobre a intensidade, ou seja, neste caso 10%
+            setMatrizDeLEDSComIntensidade(caixa_de_desenhos[num], 0.1, 0.1, 0.1);
+        }
     }
 }
