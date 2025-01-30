9674b37e87c6ad621eb90c5930d0a749ae677ad9
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-30 12:28:43 -0300

    Update Tarefa2701.c

--- Tarefa2701.c
+++ Tarefa2701.c
@@ -3,20 +3,37 @@
 
 // Configuração dos pinos
 
-const uint LED_BLUE_PIN = 11;
-const uint LED_GREEN_PIN = 12;
-const uint LED_RED_PIN = 13;
+const uint LED_RED_PIN = 13; // Green = 11, Blue = 12 and Red = 13
 
-const uint BUTTON1_PIN = 5;
-const uint BUTTON2_PIN = 6;
-
+const uint BUTTON1_PIN = 5; // Define o botão 1 como pino 5
+const uint BUTTON2_PIN = 6; // Define o botão 2 como pino 6
 
 int main()
 {
+    // Inicialização dos pinos
     stdio_init_all();
+
+    gpio_init(LED_RED_PIN);
+    gpio_init(BUTTON1_PIN);
+    gpio_init(BUTTON2_PIN);
+
+    // Confira direção dos pinos (Entrada ou saída)
+
+    gpio_set_dir(LED_RED_PIN, GPIO_OUT);
+    gpio_set_dir(BUTTON1_PIN, GPIO_IN);
+    gpio_set_dir(BUTTON2_PIN, GPIO_IN);
+
+    /* Configuração para habilitar o pull up interno para os botões, evitando o uso de resistores externos e
+    garantindo que o pino esteja em nível alto quando o botão não estiver pressionado. */
+
+    gpio_pull_up(BUTTON1_PIN);
+    gpio_pull_up(BUTTON2_PIN);
 
     while (true)
     {
+
+
+        
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
