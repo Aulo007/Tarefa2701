e31f58e97eefd9eb7214fc55c0b0b4d327763def
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-30 11:51:25 -0300

    Define pinos

--- Tarefa2701.c
+++ Tarefa2701.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Configuração dos pinos
+
+const uint LED_BLUE_PIN = 11;
+const uint LED_GREEN_PIN = 12;
+const uint LED_RED_PIN = 13;
+
+const uint BUTTON1_PIN = 5;
+const uint BUTTON2_PIN = 6;
 
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
